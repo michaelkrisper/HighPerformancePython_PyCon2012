7cb5fbafe2530d856dcc0c0e66fb3366f156e766
Michael Krisper <michael.krisper@gmail.com>
2016-07-05 21:51:10 +0200

    cython cdef

--- mandelbrot/pure_python/calculate_z.pyx
+++ mandelbrot/pure_python/calculate_z.pyx
@@ -1,11 +1,9 @@
-def calculate_z_serial_purepython(q, maxiter, z):
-    """Pure python with complex datatype, iterating over list of q and z"""
+def calculate_z_serial_purepython(list q, unsigned int maxiter, list z):
+    cdef unsigned int i
+    cdef int iteration
+
     output = [0] * len(q)
     for i in range(len(q)):
-        if i % 1000 == 0:
-            pass
-            # print out some progress info since it is so slow...
-            # print("%0.2f%% complete" % (1.0/len(q) * i * 100))
         zi = z[i]
         qi = q[i]
         for iteration in range(maxiter):
